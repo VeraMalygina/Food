cc82a666db980c731769292dcd220e147696ff87
Vera <Malygina.vera95@bk.ru>
2025-07-09 16:50:57 +0200

    Added all files from the change

---
+++ src/js/modules/cards.js
@@ -0,0 +1,73 @@
+import { getResource } from "../services/services";
+
+function cards() {
+  // Utiliser des classes pour les cartes
+  const API = "https://json-server-backend-izkc.onrender.com/";
+
+  class MenuCard {
+    constructor(src, alt, title, descr, price, parentSelector, ...classes) {
+      this.src = src;
+      this.alt = alt;
+      this.title = title;
+      this.descr = descr;
+      this.price = price;
+      this.classes = classes;
+      this.parent = document.querySelector(parentSelector);
+      this.transfer = 1.16;
+      this.changeToUAH();
+    }
+
+    changeToUAH() {
+      this.price = this.price * this.transfer;
+    }
+
+    render() {
+      const element = document.createElement("div");
+      if (this.classes.length === 0) {
+        this.classes = "menu__item";
+        element.classList.add(this.classes);
+      } else {
+        this.classes.forEach((className) => element.classList.add(className));
+      }
+
+      element.innerHTML = `
+            <img src=${this.src} alt=${this.alt}>
+            <h3 class="menu__item-subtitle">${this.title}</h3>
+            <div class="menu__item-descr">${this.descr}</div>
+            <div class="menu__item-divider"></div>
+            <div class="menu__item-price">
+                <div class="menu__item-cost">Prix:</div>
+                <div class="menu__item-total"><span>${this.price}</span> <strong>$</strong>/ jour</div>
+            </div>`;
+      this.parent.append(element);
+    }
+  }
+
+  getResource(`${API}menu`).then((data) => {
+    data.forEach(({ img, altimg, title, descr, price }) => {
+      new MenuCard(
+        img,
+        altimg,
+        title,
+        descr,
+        price,
+        ".menu .container"
+      ).render();
+    });
+  });
+
+  // axios.get(`${API}menu`).then((data) => {
+  //   data.data.forEach(({ img, altimg, title, descr, price }) => {
+  //     new MenuCard(
+  //       img,
+  //       altimg,
+  //       title,
+  //       descr,
+  //       price,
+  //       ".menu .container"
+  //     ).render();
+  //   });
+  // });
+}
+
+export default cards;
